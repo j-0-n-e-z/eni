5de6f7c45679f9e472fc1bfbabc96d9ec6f031a4
j-0-n-e-z <j000neeez@gmail.com>
2025-08-06 02:53:05 +0500

    add error class

--- src/types.ts
+++ src/types.ts
@@ -73,3 +73,13 @@ export interface LoginResponse {
 		isEmailConfirmed: boolean
 	}
 }
+
+export class ApiError extends Error {
+	constructor(
+		public readonly statusCode: number,
+		public readonly message: string,
+		public readonly field?: string
+	) {
+		super(message)
+	}
+}
