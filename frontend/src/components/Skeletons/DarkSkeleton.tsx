25592796112fddcbad2ad41d6fe6fb961c173044
j-0-n-e-z <j000neeez@gmail.com>
2025-09-29 13:39:24 +0500

    some fixes & pipeline test

--- frontend/src/components/Skeletons/DarkSkeleton.tsx
+++ frontend/src/components/Skeletons/DarkSkeleton.tsx
@@ -3,5 +3,10 @@ import ReactLoadingSkeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 export const DarkSkeleton = (props: SkeletonProps) => (
-  <ReactLoadingSkeleton {...props} containerClassName='flex1' baseColor='#2d3846' highlightColor='#364252'/>
+	<ReactLoadingSkeleton
+		{...props}
+		baseColor='#2d3846'
+		containerClassName='flex1'
+		highlightColor='#364252'
+	/>
 )
