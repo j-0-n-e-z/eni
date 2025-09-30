2a038a23199618d42c75926585f36d56e7947439
j-0-n-e-z <j000neeez@gmail.com>
2025-09-30 16:59:21 +0500

    fixed test imports

--- backend/src/__tests__/parseSrt/parseSrt.test.ts
+++ backend/src/__tests__/parseSrt/parseSrt.test.ts
@@ -1,16 +1,16 @@
 import assert from 'node:assert'
 
-import { parseSrt } from '@/utils'
+import { parseSrt } from '../../utils/srt/parseSrt'
 
 import { expected } from './expected'
 import { source } from './source'
 
-describe('concat uncompleted sentences between subtitles', () => {
-	it("should concat with no punctiation mark or ',' at the end [Avatar]", () => {
+describe('parseSrt', () => {
+	it("should concat subtitles with no punctiation mark or ',' at the end", () => {
 		const actual = parseSrt(source.avatar)
 		assert.deepEqual(actual, expected.avatar)
 	})
-	it("should concat with '...' at the end [Hobbit: An Unexpected Journey]", () => {
+	it("should concat subtitles with '...' at the end", () => {
 		const actual = parseSrt(source.hobbit)
 		assert.deepEqual(actual, expected.hobbit)
 	})
