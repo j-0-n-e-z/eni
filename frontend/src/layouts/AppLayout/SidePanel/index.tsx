2212289c2311d07c58c990b7e40c2c504b1a59dd
j-0-n-e-z <j000neeez@gmail.com>
2025-11-01 19:41:37 +0500

    refactor sidepanel

--- frontend/src/layouts/AppLayout/SidePanel/index.tsx
+++ frontend/src/layouts/AppLayout/SidePanel/index.tsx
@@ -1,16 +1,54 @@
 import cn from 'classnames'
+import { type ReactNode } from 'react'
 
+import { useAuthData } from '@/hooks'
 import {
 	BookIcon,
 	BrainIcon,
 	InfoIcon,
 	PopularIcon,
 	SearchIcon,
-	SettingsIcon
-} from '@/ui/icons'
+	SettingsIcon,
+	Skeleton
+} from '@/ui'
 
-import { SidePanel } from './SidePanel'
+import { MenuBurgerButton } from './MenuBurgerButton'
 import styles from './SidePanel.module.scss'
+import { AuthorizedItem } from './components/AuthorizedItem'
+import { BottomNavigation } from './components/BottomNavigation'
+import { NavItem } from './components/NavItem'
+import { Navigation } from './components/Navigation'
+import { ThemeToggle } from './components/ThemeToggle'
+import { TopArea } from './components/TopArea'
+
+interface SidePanelProps {
+	children: ReactNode
+}
+
+const SidePanel = ({ children }: SidePanelProps) => {
+	const { isMeFetching } = useAuthData()
+
+	if (isMeFetching)
+		return (
+			<aside className={styles.sidepanel}>
+				<Skeleton containerClassName='flex1' height='100%' variant='dark' />
+			</aside>
+		)
+
+	return (
+		<>
+			<MenuBurgerButton />
+			<aside className={styles.sidepanel}>{children}</aside>
+		</>
+	)
+}
+
+SidePanel.TopArea = TopArea
+SidePanel.Navigation = Navigation
+SidePanel.NavItem = NavItem
+SidePanel.BottomNavigation = BottomNavigation
+SidePanel.AuthorizedItem = AuthorizedItem
+SidePanel.ThemeToggle = ThemeToggle
 
 export const AppSidePanel = () => (
 	<SidePanel>
