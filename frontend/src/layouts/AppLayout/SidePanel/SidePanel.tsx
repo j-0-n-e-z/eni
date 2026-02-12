1814184b28d109631747c06b652340cf8628cabe
j-0-n-e-z <j000neeez@gmail.com>
2026-02-12 17:39:09 +0500

    refactor(SidePanel): remove useless compound

--- frontend/src/layouts/AppLayout/SidePanel/SidePanel.tsx
+++ frontend/src/layouts/AppLayout/SidePanel/SidePanel.tsx
@@ -12,7 +12,7 @@ import { NavItem } from './components/NavItem'
 import { Navigation } from './components/Navigation'
 import { ThemeToggle } from './components/ThemeToggle'
 import { TopArea } from './components/TopArea'
-import { NAV_ITEMS } from './navItems'
+import { NAV_ITEMS } from './constants/navItems'
 
 import styles from './SidePanel.module.scss'
 
@@ -27,13 +27,6 @@ const SidePanel = ({ children }: SidePanelProps) => (
 	</>
 )
 
-SidePanel.TopArea = TopArea
-SidePanel.Navigation = Navigation
-SidePanel.NavItem = NavItem
-SidePanel.BottomNavigation = BottomNavigation
-SidePanel.AuthorizedItem = AuthorizedItem
-SidePanel.ThemeToggle = ThemeToggle
-
 export const AppSidePanel = () => {
 	const { isMeFetching } = useAuthData()
 
@@ -41,28 +34,28 @@ export const AppSidePanel = () => {
 
 	return (
 		<SidePanel>
-			<SidePanel.TopArea>
+			<TopArea>
 				<Icons.BrainIcon className={cn(styles.bookIcon, styles.topLogo)} />
 				<div className={styles.topTitle}>Eni</div>
-			</SidePanel.TopArea>
+			</TopArea>
 
-			<SidePanel.Navigation>
+			<Navigation>
 				{NAV_ITEMS.map((props) => {
-					const navItem = <SidePanel.NavItem {...props} key={props.to} />
+					const navItem = <NavItem {...props} key={props.to} />
 
 					return props.authorizedOnly ? (
-						<SidePanel.AuthorizedItem key={props.to}>
+						<AuthorizedItem key={props.to}>
 							{navItem}
-						</SidePanel.AuthorizedItem>
+						</AuthorizedItem>
 					) : (
 						navItem
 					)
 				})}
-			</SidePanel.Navigation>
+			</Navigation>
 
-			<SidePanel.BottomNavigation />
+			<BottomNavigation />
 
-			<SidePanel.ThemeToggle />
+			<ThemeToggle />
 		</SidePanel>
 	)
 }
