dfc87da19fc7bdf26864f5044da86d28499b8948
j-0-n-e-z <j000neeez@gmail.com>
2026-03-14 21:09:13 +0500

    remove constants for Hero text

--- frontend/src/pages/Main/components/Hero/Hero.tsx
+++ frontend/src/pages/Main/components/Hero/Hero.tsx
@@ -2,60 +2,44 @@ import { DotLottieReact } from '@lottiefiles/dotlottie-react'
 import cn from 'classnames'
 import { motion as m } from 'framer-motion'
 
+import { fadeInAndUp, getFadeInVariants } from '@/animations/variants'
 import { Background, Icons } from '@/ui'
 
 import movieTheatre from '../../../../ui/assets/lotties/MovieTheatre.lottie'
 
-import { HERO_LINES } from './constants/hero-text'
-
 import styles from './Hero.module.scss'
-
-const itemVariants = {
-	hidden: { opacity: 0, y: 60 },
-	visible: (custom: number) => ({
-		opacity: 1,
-		transition: {
-			delay: 0.3 + custom * 0.3,
-			duration: 0.4
-		},
-		y: 0
-	})
-}
 
 interface HeroProps {
 	goToNextSlide: () => void
 }
 
+const fadeIn = getFadeInVariants()
+
 export const Hero = ({ goToNextSlide }: HeroProps) => (
 	<div className={styles.hero}>
 		<div className={styles.heroTextContainer}>
-			<m.h2 className={styles.heroHeading} custom={0} variants={itemVariants}>
+			<m.h2 className={styles.heroHeading} variants={fadeInAndUp}>
 				<Icons.Brain className={styles.heroIcon} />
 				Eni
 			</m.h2>
 
 			<ul className={styles.heroLines}>
-				{HERO_LINES.map((text, i) => (
-					<m.li key={text} custom={i + 1} variants={itemVariants}>
-						{text}
-					</m.li>
-				))}
+				<m.li custom={1} variants={fadeInAndUp}>
+					Привет
+				</m.li>
+				<m.li custom={2} variants={fadeInAndUp}>
+					Меня зовут Рустам
+				</m.li>
+				<m.li custom={3} variants={fadeInAndUp}>
+					Я Junior Frontend разработчик
+				</m.li>
+				<m.li custom={4} variants={fadeInAndUp}>
+					Это мой пет-проект для изучения английского
+				</m.li>
 			</ul>
 		</div>
 
-		<m.div
-			className={styles.heroAnimationContainer}
-			variants={{
-				hidden: { opacity: 0 },
-				visible: {
-					opacity: 1,
-					transition: {
-						delay: 0.2,
-						duration: 0.5
-					}
-				}
-			}}
-		>
+		<m.div className={styles.heroAnimationContainer} variants={fadeIn}>
 			<DotLottieReact
 				autoplay
 				loop
@@ -65,17 +49,8 @@ export const Hero = ({ goToNextSlide }: HeroProps) => (
 		</m.div>
 
 		<m.div
-			className={styles.howToUse}
-			variants={{
-				hidden: { opacity: 0 },
-				visible: {
-					opacity: 1,
-					transition: {
-						delay: 2,
-						duration: 0.5
-					}
-				}
-			}}
+			className={styles.aboutProject}
+			variants={getFadeInVariants(2)}
 			whileInView={{
 				transition: {
 					y: {
@@ -101,19 +76,19 @@ export const Hero = ({ goToNextSlide }: HeroProps) => (
 			drag
 			className={cn(styles.particle, styles.particle1)}
 			custom={5}
-			variants={itemVariants}
+			variants={fadeInAndUp}
 		/>
 		<m.div
 			drag
 			className={cn(styles.particle, styles.particle2)}
 			custom={6}
-			variants={itemVariants}
+			variants={fadeInAndUp}
 		/>
 		<m.div
 			drag
 			className={cn(styles.particle, styles.particle3)}
 			custom={7}
-			variants={itemVariants}
+			variants={fadeInAndUp}
 		/>
 	</div>
 )
