ea10ac0af041411b42c0b236a0f1c2700cc2bc6a
j-0-n-e-z <j000neeez@gmail.com>
2026-03-14 21:24:17 +0500

    make Button styles global
    so they could be used for <a> as well

--- frontend/src/ui/components/Button/Button.tsx
+++ frontend/src/ui/components/Button/Button.tsx
@@ -1,8 +1,6 @@
 import cn from 'classnames'
 import React from 'react'
 
-import styles from './Button.module.scss'
-
 type ButtonVariant = 'contained' | 'outlined' | 'text'
 
 interface ButtonProps extends React.ComponentProps<'button'> {
@@ -14,7 +12,7 @@ interface ButtonProps extends React.ComponentProps<'button'> {
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 	({ children, variant = 'contained', className, loading, ...props }, ref) => (
 		<button
-			className={cn(styles.button, styles[variant], className)}
+			className={cn('button', `button--${variant}`, className)}
 			type='button'
 			{...props}
 			ref={ref}
